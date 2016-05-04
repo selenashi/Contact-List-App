97e563c018f4b2b760217b593be1795763d21f0f
selenashi <shiming.shi0718@gmail.com>
2016-05-04 05:17:46 +0000

    completed all tasks contact_list.rb

--- contact.rb
+++ contact.rb
@@ -9,8 +9,15 @@ class Contact
   # Creates a new contact object
   # @param name [String] The contact's name
   # @param email [String] The contact's email address
-  def initialize(name, email)
+  def initialize(id, name, email)
+    @id = id
+    @name = name
+    @email = email
     # TODO: Assign parameter values to instance variables.
+  end
+
+  def to_s
+    @id.to_s + ': ' + @name + ' (' + @email + ')'
   end
 
   # Provides functionality for managing contacts in the csv file.
@@ -20,19 +27,39 @@ class Contact
     # @return [Array<Contact>] Array of Contact objects
     def all
       # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+      contacts = CSV.read('contacts.csv')
+      contacts = contacts.map {|contact| self.new(contact[0], contact[1], contact[2])}
+      puts contacts
+      puts "\n#{contacts.length} records total"  
     end
 
     # Creates a new contact, adding it to the csv file, returning the new contact.
     # @param name [String] the new contact's name
     # @param email [String] the contact's email
-    def create(name, email)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+    def create(id, name, email) 
+      CSV.open('contacts.csv', 'a+t') do |contacts|
+        contacts << [id, name, email]
+      end
+      Contact.new(id, name, email)
     end
+
+      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+   
     
     # Find the Contact in the 'contacts.csv' file with the matching id.
     # @param id [Integer] the contact id
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
+      contacts = CSV.read('contacts.csv')
+      found = false  
+      contacts.each do |contact|
+        if contact[0] == id.to_s
+          contact = Contact.new(contact[0], contact[1], contact[2])
+          puts contact
+          found = true
+        end 
+      end
+      puts "not found" if found == false
       # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
     end
     
@@ -40,9 +67,23 @@ class Contact
     # @param term [String] the name fragment or email fragment to search for
     # @return [Array<Contact>] Array of Contact objects.
     def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+      found = false
+      contacts = CSV.read('contacts.csv')
+      contacts.each do |contact|
+        if contact.join.include?(term)
+          contact = Contact.new(contact[0], contact[1], contact[2])
+          puts contact
+          found = true 
+        end
+      end
+      puts "not found" if found == false
     end
 
+      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
   end
+end 
 
-end
+# puts Contact.all
+# puts Contact.create("4", "Shiming", "shi_shiming@hotmail.com")
+# Contact.find(1)
+
--- contact_list.rb
+++ contact_list.rb
@@ -3,6 +3,42 @@ require_relative 'contact'
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  class << self
 
+    def menu
+      "Here is a list of available commands:
+        new    - Create a new contact
+        list   - List all contacts
+        show   - Show a contact
+        search - Search contacts"
+    end 
+
+    def new_id
+      puts "Please enter the id of the contact"
+      id_input = STDIN.gets.chomp
+      return id_input.to_s
+    end
+
+    def new_name 
+      puts "Please enter the full name of the contact"
+      name_input = STDIN.gets.chomp
+      return name_input.to_s
+    end
+
+    def new_email
+      puts "Please enter the email of the contact"
+      email_input = STDIN.gets.chomp
+      return email_input.to_s
+    end
+  end
 end
+
+  case ARGV[0]
+    when 'list' then Contact.all
+    when 'new' then Contact.create(ContactList.new_id, ContactList.new_name, ContactList.new_email)
+    when 'show' then Contact.find(ARGV[1])
+    when 'search' then Contact.search(ARGV[1])
+    else
+      puts ContactList.menu
+    end
+
